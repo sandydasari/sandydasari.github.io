f6198c9794a6f504d83ddb7e14f3a0253190fcd5
sandydasari <sandydasari977@gmail.com>
2025-07-25 20:33:27 +0530

    fix side socialmedia

--- components/Home/SocialMediaArround/SocialMediaArround.tsx
+++ components/Home/SocialMediaArround/SocialMediaArround.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { motion } from "../../../node_modules/framer-motion/dist/framer-motion";
+import { motion } from "framer-motion";
 import GithubIcon from "../../Icons/GithubIcon";
 import LinkedinIcon from "../../Icons/LinkedinIcon";
 import InstagramIcon from "../../Icons/InstagramIcon";
